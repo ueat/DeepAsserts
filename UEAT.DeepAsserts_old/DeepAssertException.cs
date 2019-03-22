d1b65e1ce8a36e42dfcba148c7af4f704a9b7e09
Benoit Tremblay <trembl.ben@gmail.com>
2019-03-22 18:13:46 -0400

    Publish on .NET Standard 2

---
+++ UEAT.DeepAsserts_old/DeepAssertException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UEAT.DeepAsserts
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Deep equality has failed. The details of the diff is within the exception message.
+    /// </summary>
+    public class DeepAssertException : Exception
+    {
+        internal DeepAssertException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
